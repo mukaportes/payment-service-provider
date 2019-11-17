a61c0a1aa46c538ee3f2757fc63255f011fe7662
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-17 13:30:14 -0300

    Add additional db config

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -2,8 +2,17 @@ module.exports = {
   db: {
     database: process.env.DATABASE_NAME,
     dialect: 'postgres',
+    dialectOptions: {
+      multipleStatements: true,
+    },
     host: process.env.DATABASE_HOST,
+    logging: console.log,
     password: process.env.DATABASE_PASSWORD,
+    pool: {
+      idle: process.env.DATABASE_IDLE_TIME || 10000,
+      max: process.env.DATABASE_MAX_POOL || 200,
+      min: process.env.DATABASE_MIN_POOL || 50,
+    },
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
   },
