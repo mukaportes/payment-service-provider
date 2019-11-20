3677adbcac4f1641d1a29e7a4895a83808660b36
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-20 13:33:23 -0300

    created CustomerCreate controller and service

---
+++ src/domain/services/customers/create.js
@@ -0,0 +1,20 @@
+const httpResponse = require('../../../helpers/http-response');
+
+module.exports = function(CustomerEntity, customerModel) {
+  this.execute = (input) => {
+    const customerEntityInstance = new CustomerEntity(input);
+    const { isValid, validationErrors } = customerEntityInstance.validateNewCustomer(input);
+
+    if (!isValid) return httpResponse.badRequest({ errors: validationErrors });
+
+    const newCustomer = customerEntityInstance.getNewCustomer();
+
+    return customerModel.create(newCustomer)
+      // TODO: add sys messages to response 
+      .then(() => httpResponse.success({ data: 'Customer successfully created' }))
+      .catch((error) => httpResponse.unprocessableEntity({
+        error: error.message,
+        stack: error.stack,
+      }));
+  };
+};
---
+++ src/infrastructure/controllers/customers/create.js
@@ -0,0 +1,13 @@
+const models = require('../../../db/models');
+const CustomerEntity = require('../../../domain/entities/customers');
+const CustomerCreateService = require('../../../domain/services/customers/create');
+
+module.exports = function(req, res) {
+  const service = new CustomerCreateService(CustomerEntity, models.customer);
+  const { birthday, country, name } = req.body;
+
+  // mudar pro execute ser sincrono e ter os then/catch dentro dele
+  service.execute({ birthday, country, name })
+    .then(({ output, status }) => res.status(status).json(output));
+};
+
