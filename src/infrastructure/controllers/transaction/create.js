072ef740906ec015394fa2970f63e141aca58e53
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-23 14:20:17 -0300

    Add createTransaction controller

---
+++ src/infrastructure/controllers/transaction/create.js
@@ -0,0 +1,21 @@
+const TransactionEntity = require('../../../domain/entities/transaction');
+const PayableEntity = require('../../../domain/entities/payable');
+const models = require('../../../db/models');
+const TransactionCreateService = require('../../../domain/services/transaction/create');
+
+module.exports = (req, res) => {
+  const service = new TransactionCreateService({
+    PayableEntity,
+    TransactionEntity,
+    transactionModel: models.transaction,
+  });
+  const {
+    amount, cardCvv, cardExpirationDate, cardHolderName,
+    cardNumber, customerUid, description, paymentMethod,
+  } = req.body;
+
+  service.execute({
+    amount, cardCvv, cardExpirationDate, cardHolderName,
+    cardNumber, customerUid, description, paymentMethod,
+  }).then(({ output, status }) => res.status(status).json(output));
+};
