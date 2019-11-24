ed9d2a4ce6cc8f717ee31ad5541af473115eb2af
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 13:25:40 -0300

    Updated payable entity

--- src/domain/entities/payable/index.js
+++ src/domain/entities/payable/index.js
@@ -2,8 +2,8 @@ const uuid = require('uuid/v4');
 const PAYABLE_STATUS = require('../../../enums/payable-status');
 
 // eslint-disable-next-line max-lines-per-function 
-module.exports = function(transaction) {
-  this.validate = function() {
+module.exports = function (transaction) {
+  this.validate = function () {
     const errors = [];
 
     if (!PAYABLE_STATUS[transaction.paymentMethod]) {
@@ -12,29 +12,43 @@ module.exports = function(transaction) {
 
     return errors;
   };
-  
-  this.getStatus = function() {
+
+  this.getStatus = function () {
     return PAYABLE_STATUS[transaction.paymentMethod];
   };
 
-  this.getCreationDate = function() {
+  this.getCreationDate = function () {
     const currentDate = new Date();
     const status = this.getStatus();
 
     if (status === PAYABLE_STATUS.credit_card) {
       const daysToAdd = 30;
-      currentDate.setDate(currentDate.getDate() + daysToAdd); 
+      currentDate.setDate(currentDate.getDate() + daysToAdd);
     }
 
     return currentDate.toISOString();
   };
 
-  this.getNewPayable = function() {
+  this.getFeeAndAmount = function () {
+    const status = this.getStatus();
+    const creditCardPercentual = 5;
+    const debitCardPercentual = 3;
+    const feePercentual = status === PAYABLE_STATUS.credit_card
+      ? creditCardPercentual : debitCardPercentual;
+
+    return {
+      amount: transaction.amount * (1 - (feePercentual * 0.01)),
+      fee: transaction.amount * (feePercentual * 0.01),
+    };
+  };
+
+  this.getNewPayable = function () {
     return {
       createdAt: this.getCreationDate(),
       createdBy: transaction.customerUid,
       payableUid: uuid(),
       status: this.getStatus(),
+      ...this.getFeeAndAmount(),
     };
   };
 };
