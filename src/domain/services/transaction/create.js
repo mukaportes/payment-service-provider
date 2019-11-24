23e1527558d857c5717275f5eac3541fefc8f9ad
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 13:26:57 -0300

    Fixed create transaction service create method

--- src/domain/services/transaction/create.js
+++ src/domain/services/transaction/create.js
@@ -1,13 +1,13 @@
 const httpResponse = require('../../../helpers/http-response');
 
 module.exports = function ({
-  PayableEntity, TransactionEntity, transactionModel,
+  PayableEntity, TransactionEntity, payableModel, transactionModel,
 }) {
-  this.execute = function(input) {
+  this.execute = function (input) {
     const transactionEntityInstance = new TransactionEntity(input);
     const payableEntityInstance = new PayableEntity(input);
 
-    const transactionValidation = transactionEntityInstance.validate();
+    const transactionValidation = transactionEntityInstance.validateNewTransaction();
 
     if (!transactionValidation.isValid) {
       return httpResponse.badRequest({ errors: transactionValidation });
@@ -18,7 +18,9 @@ module.exports = function ({
       payable: payableEntityInstance.getNewPayable(),
     };
 
-    return transactionModel.create(newTransaction)
+    return transactionModel.create(newTransaction, {
+      include: [{ as: 'payable', model: payableModel }],
+    })
       // TODO: add sys messages to response
       .then((transaction) => httpResponse.success({
         data: 'Transaction successfully created',
