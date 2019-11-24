8a600ed75f1698e6897d67c2a8def381cd01d5c7
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 13:27:23 -0300

    Add transaction router to build routes

--- src/loaders/build-routes.js
+++ src/loaders/build-routes.js
@@ -1,7 +1,9 @@
 const customer = require('../infrastructure/routes/customer');
+const transaction = require('../infrastructure/routes/transaction');
 
 const buildRoutes = (app) => {
   app.use('/v1/customer', customer());
+  app.use('/v1/transaction', transaction());
 
   return app;
 };
