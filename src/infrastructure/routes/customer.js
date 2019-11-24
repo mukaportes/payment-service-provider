2690fe68a39ff496da2a034d97df146318c888fa
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 14:55:28 -0300

    Created CustomerBalance controller and route

--- src/infrastructure/routes/customer.js
+++ src/infrastructure/routes/customer.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const customerCreateController = require('../controllers/customers/create');
+const customerBalanceController = require('../controllers/customers/balance');
 
 const customerRouter = express.Router();
 
 const router = () => {
   customerRouter.route('/').post(customerCreateController);
+  customerRouter.route('/:customerUid/balance').get(customerBalanceController);
 
   return customerRouter;
 };
