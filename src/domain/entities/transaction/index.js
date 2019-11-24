0b0e562463524a8610de1558cbac4568148b9478
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 13:25:59 -0300

    Updated transaction entity

--- src/domain/entities/transaction/index.js
+++ src/domain/entities/transaction/index.js
@@ -1,20 +1,22 @@
 const uuid = require('uuid/v4');
 const validate = require('../../../helpers/validator');
 const transactionValidations = require('./validations');
+const PAYMENT_METHODS = require('../../../enums/payment-methods');
 
 module.exports = function(transaction) {
-  this.validateNewCustomer = function() {
+  this.validateNewTransaction = function() {
+    // TODO: add validation of cardExpirationDate >= new Date()
     return validate(transaction, transactionValidations.create);
   };
 
   // TODO: review this logic below
-  this.sanitizeCardNumber = function() {
+  this.getSanitizedCardNumber = function() {
     const cardNumberToString = transaction.cardNumber.toString();
     const cardNumberLength = cardNumberToString.length - 1;
     let sanitizedCardNumber = '';
 
-    for (let counter = cardNumberLength; counter <= (cardNumberLength - 3); counter += 1) {
-      sanitizedCardNumber += cardNumberToString[counter]; 
+    for (let counter = cardNumberLength; counter >= (cardNumberLength - 3); counter -= 1) {
+      sanitizedCardNumber = cardNumberToString[counter] + sanitizedCardNumber; 
     }
 
     return Number(sanitizedCardNumber);
@@ -23,9 +25,11 @@ module.exports = function(transaction) {
   this.getNewTransaction = function(payable) {
     return {
       ...transaction,
+      cardNumber: this.getSanitizedCardNumber(),
       createdAt: new Date().toISOString(),
       createdBy: transaction.customerUid,
       payable,
+      paymentMethod: PAYMENT_METHODS.toNumber[transaction.paymentMethod],
       transactionUid: uuid(),
     };
   };
