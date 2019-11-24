de6bfb06d7444aa1e8402cc099fed10e3676b93f
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-24 13:26:32 -0300

    Updated enums

---
+++ src/enums/payment-methods.js
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+
+const toString = {
+  1: 'credit_card',
+  2: 'debit_card',
+};
+
+const toNumber = {
+  credit_card: 1,
+  debit_card: 2,
+};
+
+module.exports = {
+  toNumber,
+  toString,
+};
