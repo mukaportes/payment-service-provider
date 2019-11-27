f0e357dd5392273b94ee70377417d1f1b94fd0a6
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-26 22:40:48 -0300

    Add dockerfile and compose

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,11 +1,11 @@
 module.exports = {
   client: 'pg',
   connection: {
-    database: process.env.DATABASE_NAME || 'psp_db',
-    host: process.env.DATABASE_HOST || 'localhost',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    port: process.env.DATABASE_PORT || 5432,
-    user: process.env.DATABASE_USER || 'postgres',
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
   },
   debug: true,
   migrations: {
