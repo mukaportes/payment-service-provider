5d06ecf9f749ceb337a6173002d2eebbf28c53da
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-27 23:51:31 -0300

    Add swagger route and docs file

--- src/loaders/build-routes.js
+++ src/loaders/build-routes.js
@@ -1,9 +1,12 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../docs');
 const customer = require('../infrastructure/routes/customer');
 const transaction = require('../infrastructure/routes/transaction');
 
 const buildRoutes = (app) => {
   app.use('/v1/customer', customer());
   app.use('/v1/transaction', transaction());
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   return app;
 };
