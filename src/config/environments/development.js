7da5d90aea3025ce7633e121644349f41755ccd4
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-29 22:32:24 -0300

    Fixed db pool min max config

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -10,8 +10,8 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     pool: {
       idle: process.env.DATABASE_IDLE_TIME || 10000,
-      max: process.env.DATABASE_MAX_POOL || 200,
-      min: process.env.DATABASE_MIN_POOL || 50,
+      max: process.env.DATABASE_MAX_POOL || 30,
+      min: process.env.DATABASE_MIN_POOL || 10,
     },
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
