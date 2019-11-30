bcaf374fcf106bd4a6172992ae6362a10f8cc181
Murilo Portescheller <murilo.portescheller@avanade.com>
2019-11-30 00:36:29 -0300

    Add customerUid to create service return

--- src/domain/services/customer/create.js
+++ src/domain/services/customer/create.js
@@ -1,7 +1,7 @@
 const httpResponse = require('../../../helpers/http-response');
 const MESSAGES = require('../../messages');
 
-module.exports = function(CustomerEntity, customerModel) {
+module.exports = function ({ CustomerEntity, customerModel }) {
   this.execute = (input) => {
     const customerEntityInstance = new CustomerEntity(input);
     const { isValid, validationErrors } = customerEntityInstance.validateNewCustomer(input);
@@ -11,7 +11,12 @@ module.exports = function(CustomerEntity, customerModel) {
     const newCustomer = customerEntityInstance.getNewCustomer();
 
     return customerModel.create(newCustomer)
-      .then(() => httpResponse.success(MESSAGES.customers.create.success))
+      .then((customerCreated) => {
+        return httpResponse.success({
+          ...MESSAGES.customers.create.success,
+          customerUid: customerCreated.customerUid, 
+        });
+      })
       .catch((error) => httpResponse.unprocessableEntity({
         error: error.message,
         stack: error.stack,
